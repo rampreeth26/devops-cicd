b2f2a7187af9c4c8f07761a059eef78b42b50d43
rampreeth26 <122955891+rampreeth26@users.noreply.github.com>
2024-11-05 12:23:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more react + CICD
+          Learn more react 
         </a>
       </header>
     </div>
